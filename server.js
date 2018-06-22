380184d9b7db6f980d26780713b69c14ab7e1c61
Rohith GB <rohitgbg@gmail.com>
2018-06-22 18:09:46 +0530

    express server setup

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var port = Number(process.env.PORT || 4200);
+var port = Number(process.env.PORT || 3000);
 
-app.use(express.static(__dirname+ '/dist'));
+app.use(express.static(__dirname+ '/dist/github-search'));
 
 app.get('/*', (req, res, next)=>{
-    res.sendFile(path.join(__dirname +'/dist/index.html'));
+    res.sendFile(path.join(__dirname +'/dist/github-search/index.html'));
 })
 
 
