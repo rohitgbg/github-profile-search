0506a4771ad2a21d3190033f313c649a8bd32488
Rohith GB <rohitgbg@gmail.com>
2018-06-22 17:52:46 +0530

    added express server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = Number(process.env.PORT || 4200);
 app.use(express.static(__dirname+ '/dist'));
 
 app.get('/*', (req, res, next)=>{
-    res.sendFile(path.join(__dirname +'/dist/index.ht,l'));
+    res.sendFile(path.join(__dirname +'/dist/index.html'));
 })
 
 
